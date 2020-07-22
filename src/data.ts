035a67bfda18d172e7f30b40bf08120e37ae05d8
Jabster28 <justynboyer@gmail.com>
2020-07-22 11:04:31 +0100

    feat: add some extra funcs

--- src/data.ts
+++ src/data.ts
@@ -1,5 +1,6 @@
 import * as Discord from 'discord.js';
 import * as toHex from 'colornames';
+import * as Search from 'fuzzy-search';
 declare type command = {
   run: (
     msg: Discord.Message,
@@ -11,9 +12,9 @@ declare type command = {
 };
 // {[key: string]: command}
 const prefix = '-';
-const myID = 'myIDHere'; // TODO: replace with your Snowflake
-const muteRole = 'mutedRoleIDHere'; // TODO: replace with Snowflake of mute role
-const adminRole = 'mutedRoleIDHere'; // TODO: replace with Snowflake of admin role
+const myID = '350930610719817728';
+const muteRole = '710057816718573569';
+const adminRole = '711606507237671022';
 export const data: {
   hiddencommands: {[key: string]: command};
   commands: {[key: string]: command};
@@ -22,68 +23,285 @@ export const data: {
   prefix: prefix,
   hiddencommands: {
     coffee: {
-      // makes you admin :)
       run: async function (msg: Discord.Message) {
-        console.log(msg.member?.id);
         if (msg.member?.id === myID) {
-          msg.member?.roles.add(adminRole);
-          console.log(msg.guild?.roles);
+          const user = msg.mentions.members
+            ? msg.mentions.members.first()
+            : msg.member;
+
+          user?.roles.add(adminRole).catch(e => {
+            const embed = new Discord.MessageEmbed();
+            embed.setColor(toHex('red')!);
+            embed.setTitle('Error:');
+            embed.addField(e.name, e.message);
+            embed.setDescription(
+              `Please tag <@${myID}> and let them know about this.`
+            );
+            embed.setFooter('Bot made by Jabster28#6048');
+            msg.channel.send(embed);
+          });
         }
+      },
+    },
+    deletus: {
+      run: async function (msg: Discord.Message) {
+        const user = msg.mentions.members
+          ? msg.mentions.members.first()
+          : msg.member;
+
+        user?.roles.remove(adminRole).catch(e => {
+          const embed = new Discord.MessageEmbed();
+          embed.setColor(toHex('red')!);
+          embed.setTitle('Error:');
+          embed.addField(e.name, e.message);
+          embed.setDescription(
+            `Please tag <@${myID}> and let them know about this.`
+          );
+          embed.setFooter('Bot made by Jabster28#6048');
+          msg.channel.send(embed);
+        });
+      },
+    },
+    say: {
+      run: async function (msg: Discord.Message) {
+        msg.delete();
+        msg.channel.send(msg.content.substring(4));
+      },
+    },
+    gay: {
+      run: async function (msg: Discord.Message) {
+        msg.reply('no u lmao');
       },
     },
   },
   commands: {
-    shh: {
-      desc: 'Sleepy time for you. Requires manage roles permission.',
+    mute: {
+      desc: 'Puts the user with the rest of the dead memes.',
       args: '(@user)',
       run: async function (msg, args) {
-        let reason;
+        let reason: string;
         if (!msg.member?.hasPermission('MANAGE_ROLES')) {
-          msg.channel.send("Woah, you're not a mod, dude. Sorry :/");
+          msg.channel.send('Bruh, only mods can do that.');
+          return;
         }
         if (args?.length > 1) {
-          reason = ' Reason: <' + args?.splice(1).join(' ') + '>';
+          reason = ' Reason: ' + args?.splice(1).join(' ');
         } else {
           reason = '';
         }
         const usr = msg.mentions.members?.first();
-        console.log(usr);
-        usr?.roles.set([muteRole]);
-        const embed = new Discord.MessageEmbed();
-        embed.setColor(toHex('mistyrose')!);
-        embed.setTitle('_Go to sleep, ' + usr?.user.username + '_');
-        embed.setDescription(
-          `${usr?.user.tag} has been put to sleep.${reason}`
+        usr?.roles
+          .add(muteRole)
+          .then(() => {
+            if (usr?.voice.channel) usr?.voice.kick(reason);
+            const embed = new Discord.MessageEmbed();
+            embed.setColor(toHex('yellow')!);
+            embed.setTitle('Back to the graveyard, ' + usr?.user.username);
+            embed.setDescription(
+              `<@${usr?.user.id}> has been muted by <@${msg.author.id}>. ${reason}`
+            );
+            embed.setFooter('Bot made by Jabster28#6048');
+            msg.channel.send(embed);
+          })
+          .catch(e => {
+            const embed = new Discord.MessageEmbed();
+            embed.setColor(toHex('red')!);
+            embed.setTitle('Error:');
+            embed.addField(e.name, e.message);
+            embed.setDescription(
+              `Please tag <@${myID}> and let them know about this.`
+            );
+            embed.setFooter('Bot made by Jabster28#6048');
+            msg.channel.send(embed);
+          });
+      },
+    },
+    unmute: {
+      desc: 'Unmutes the user.',
+      args: '(@user)',
+      run: async function (msg) {
+        if (!msg.member?.hasPermission('MANAGE_ROLES')) {
+          msg.channel.send('Bruh, only mods can do that.');
+          return;
+        }
+        const usr = msg.mentions.members?.first();
+        usr?.roles
+          .remove(muteRole)
+          .then(() => {
+            const embed = new Discord.MessageEmbed();
+            embed.setColor(toHex('green')!);
+            embed.setTitle(
+              'Get up, ' +
+                usr?.user.username +
+                ", you've been given another chance."
+            );
+            embed.setDescription(`<@${usr?.user.id}> has been unmuted.`);
+            embed.setFooter('Bot made by Jabster28#6048');
+            msg.channel.send(embed);
+          })
+          .catch(e => {
+            const embed = new Discord.MessageEmbed();
+            embed.setColor(toHex('red')!);
+            embed.setTitle('Error:');
+            embed.addField(e.name, e.message);
+            embed.setDescription(
+              `Please tag <@${myID}> and let them know about this.`
+            );
+            embed.setFooter('Bot made by Jabster28#6048');
+            msg.channel.send(embed);
+          });
+      },
+    },
+    kick: {
+      desc: 'Kicks the user. Also circumsises them.',
+      args: '(@user)',
+      run: async function (msg, args) {
+        if (!msg.member?.hasPermission('MANAGE_ROLES')) {
+          msg.channel.send(
+            "Woah, if we let you do that we'd have a problem on our hands."
+          );
+          return;
+        }
+        let reason: string;
+        if (args?.length > 1) {
+          reason = 'Reason: ' + args?.splice(1).join(' ');
+        } else {
+          reason = '';
+        }
+        const usr = msg.mentions.members?.first();
+        if (usr?.id === myID) {
+          msg.channel.send('Sorry, Jab, but rules are rules.');
+        }
+        const embedd = new Discord.MessageEmbed();
+        embedd.setColor(toHex('orange')!);
+        embedd.setTitle(
+          "You've been kicked. Try not to make that happen again."
         );
-        embed.setFooter('Bot made by Jabster28#6048');
-        msg.channel.send(embed);
+        embedd.setDescription(
+          `You were kicked by <@${msg.author.id}>. ${reason}`
+        );
+        embedd.setFooter('Bot made by Jabster28#6048');
+        usr?.send(embedd);
+        msg.guild?.systemChannel
+          ?.createInvite({
+            maxUses: 1,
+            maxAge: 0,
+          })
+          .then(i => {
+            usr
+              ?.send(
+                `Here's a temporary invite, in case you want to join back.\n\n${i}`
+              )
+              .then(() => {
+                usr
+                  ?.kick()
+                  .then(() => {
+                    const embed = new Discord.MessageEmbed();
+                    embed.setColor(toHex('orange')!);
+                    embed.setTitle(
+                      'Whoops, ' +
+                        usr?.user.username +
+                        ' has been temporarly circumsised. RIP.'
+                    );
+                    embed.setDescription(
+                      `<@${usr?.user.id}> has been kicked. ${reason}`
+                    );
+                    embed.setFooter('Bot made by Jabster28#6048');
+                    msg.channel.send(embed);
+                  })
+                  .catch(e => {
+                    const embed = new Discord.MessageEmbed();
+                    embed.setColor(toHex('red')!);
+                    embed.setTitle('Error:');
+                    embed.addField(e.name, e.message);
+                    embed.setDescription(
+                      `Please tag <@${myID}> and let them know about this.`
+                    );
+                    embed.setFooter('Bot made by Jabster28#6048');
+                    msg.channel.send(embed);
+                  });
+              });
+          });
+      },
+    },
+    ban: {
+      desc: 'Bans the user. Also castrates them.',
+      args: '(@user)',
+      run: async function (msg, args) {
+        if (!msg.member?.hasPermission('MANAGE_ROLES')) {
+          msg.channel.send(
+            "Woah, if we let you do that we'd have a problem on our hands."
+          );
+          return;
+        }
+        let reason: string;
+        if (args?.length > 1) {
+          reason = ' Reason: ' + args?.splice(1).join(' ');
+        } else {
+          reason = '';
+        }
+        const usr = msg.mentions.members?.first();
+        usr
+          ?.ban({
+            reason: reason,
+          })
+          .then(() => {
+            const embed = new Discord.MessageEmbed();
+            embed.setColor(toHex('red')!);
+            embed.setTitle(
+              'Whoops, ' +
+                usr?.user.username +
+                ' has been permanently castrated. RIP.'
+            );
+            embed.setDescription(
+              `<@${usr?.user.id}> has been banned. ${reason}`
+            );
+            embed.setFooter('Bot made by Jabster28#6048');
+            msg.channel.send(embed);
+          })
+          .catch(e => {
+            const embed = new Discord.MessageEmbed();
+            embed.setColor(toHex('red')!);
+            embed.setTitle('Error:');
+            embed.addField(e.name, e.message);
+            embed.setDescription(
+              `Please tag <@${myID}> and let them know about this.`
+            );
+            embed.setFooter('Bot made by Jabster28#6048');
+            msg.channel.send(embed);
+          });
       },
     },
     help: {
-      desc: 'Shows all commands.',
-      run: function (msg: Discord.Message) {
-        return new Promise(res => {
-          const embed = new Discord.MessageEmbed();
-          embed.setColor(toHex('orchid')!);
-          embed.setTitle('Hi!'); // TODO: replace with bot name
-          embed.setDescription(
-            "Hi there! I'm Yui, and I'm here to help you with anything you may need!"
-          );
-          const ref: {[key: string]: command} = data.commands;
-          let k: string;
-          let ref1;
+      desc: 'Shows all commands. (and searches arguments)',
+      args: '(command)',
+      run: async function (msg, args) {
+        const x = args?.join(' ');
+        const embed = new Discord.MessageEmbed();
+        embed.setColor(toHex('cobaltgreen')!);
+        embed.setTitle('PP');
+        embed.setDescription(
+          'The bot with the second biggest PP (first is mee6)'
+        );
+        const ref: {[key: string]: command} = data.commands;
+        let k: string;
+        if (!x) {
           for (k in ref) {
             const v = ref[k];
-            embed.addField(
-              prefix + k + ' ' + ((ref1 === v.args) !== undefined ? ref1 : ' '),
-              v.desc
-            );
+            embed.addField(prefix + k + ' ' + (v.args || ' '), v.desc);
           }
-          embed.setFooter(
-            'Arguments in (parentheses) are required, and arguments in [brackets] are optional and will default to the {braces} option.\nBot made by Jabster28#6048'
-          );
-          msg.channel.send(embed).then(res);
-        });
+        } else {
+          const searcher = new Search(Object.keys(ref));
+          const res = searcher.search(x);
+          for (k in res) {
+            const v = ref[res[k]];
+            embed.addField(prefix + res[k] + ' ' + (v.args || ' '), v.desc);
+          }
+        }
+        embed.setFooter(
+          'Arguments in (parentheses) are required, and arguments in [brackets] are optional and will default to the {braces} option.\nBot made by Jabster28#6048'
+        );
+        return msg.channel.send(embed);
       },
     },
   },
